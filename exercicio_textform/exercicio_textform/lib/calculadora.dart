baf2586e36f72ed0f0151e7c215797a8980b7394
Chatheus <rtromero.sp@gmail.com>
2023-06-06 16:47:08 -0300

    Exercicio calculadora

--- exercicio_textform/exercicio_textform/lib/calculadora.dart
+++ exercicio_textform/exercicio_textform/lib/calculadora.dart
@@ -24,97 +24,104 @@ class _MyCalcState extends State<MyCalc> {
         title: Text("Operações"),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(15)),
-            Text(
-              "Operações para aprendizado do uso do Widget TextField",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-            TextField(
-              controller: campoTexto,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.blue[200],
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                labelText: 'Digite o valor 1',
-              ),
-              style: TextStyle(fontSize: 12, color: Colors.black),
-            ),
-            TextField(
-              controller: campoTextoD,
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.blue[200],
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  labelText: 'Digite o valor 2'),
-              style: TextStyle(fontSize: 12, color: Colors.black),
-            ),
-            Row(
+      body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: 75, width: 25),
-                ElevatedButton(
-                    onPressed: () {
-                      double num1 = double.tryParse(campoTexto.text) ?? 0;
-                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
-                      double resultado = num1 + num2;
-                      setState(() {
-                        _resultado = resultado;
-                      });
-                    },
-                    child: Text("+")),
-                Padding(padding: EdgeInsets.only(right: 15)),
-                ElevatedButton(
-                    onPressed: () {
-                      double num1 = double.tryParse(campoTexto.text) ?? 0;
-                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
-                      double resultado = num1 - num2;
-                      setState(() {
-                        _resultado = resultado;
-                      });
-                    },
-                    child: Text("-")),
-                Padding(padding: EdgeInsets.only(right: 15)),
-                ElevatedButton(
-                    onPressed: () {
-                      double num1 = double.tryParse(campoTexto.text) ?? 0;
-                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
-                      double resultado = num1 * num2;
-                      setState(() {
-                        _resultado = resultado;
-                      });
-                    },
-                    child: Text("*")),
-                Padding(padding: EdgeInsets.only(right: 15)),
-                ElevatedButton(
-                    onPressed: () {
-                      double num1 = double.tryParse(campoTexto.text) ?? 0;
-                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
-                      double resultado = num1 / num2;
-                      setState(() {
-                        _resultado = resultado;
-                      });
-                    },
-                    child: Text("/")),
-                Padding(padding: EdgeInsets.only(right: 15)),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _resultado = 0;
-                      });
-                    },
-                    child: Text("CE"))
+                Text(
+                  "Operações para aprendizado do uso do Widget TextField",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: campoTexto,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blue[200],
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    labelText: 'Digite o valor 1',
+                  ),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                ),
+                Padding(padding: EdgeInsets.only(top: 15)),
+                TextField(
+                  controller: campoTextoD,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.blue[200],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      labelText: 'Digite o valor 2'),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
+                ),
+                Row(
+                  children: [
+                    SizedBox(height: 75),
+                    ElevatedButton(
+                        onPressed: () {
+                          double num1 = double.tryParse(campoTexto.text) ?? 0;
+                          double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                          double resultado = num1 + num2;
+                          setState(() {
+                            _resultado = resultado;
+                          });
+                        },
+                        child: Text("+")),
+                    Padding(padding: EdgeInsets.only(right: 15)),
+                    ElevatedButton(
+                        onPressed: () {
+                          double num1 = double.tryParse(campoTexto.text) ?? 0;
+                          double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                          double resultado = num1 - num2;
+                          setState(() {
+                            _resultado = resultado;
+                          });
+                        },
+                        child: Text("-")),
+                    Padding(padding: EdgeInsets.only(right: 15)),
+                    ElevatedButton(
+                        onPressed: () {
+                          double num1 = double.tryParse(campoTexto.text) ?? 0;
+                          double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                          double resultado = num1 * num2;
+                          setState(() {
+                            _resultado = resultado;
+                          });
+                        },
+                        child: Text("*")),
+                    Padding(padding: EdgeInsets.only(right: 15)),
+                    ElevatedButton(
+                        onPressed: () {
+                          double num1 = double.tryParse(campoTexto.text) ?? 0;
+                          double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                          double resultado = num1 / num2;
+                          setState(() {
+                            _resultado = resultado;
+                          });
+                        },
+                        child: Text("/")),
+                    Padding(padding: EdgeInsets.only(right: 15)),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            campoTexto.text = '';
+                            campoTextoD.text = '';
+                            _resultado = 0;
+                          });
+                        },
+                        child: Text("CE"))
+                  ],
+                ),
+                Text('Resultado: $_resultado')
               ],
             ),
-            Text('Resultado: $_resultado')
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
