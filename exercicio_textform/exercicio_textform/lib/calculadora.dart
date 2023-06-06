9217d885c9dd319cee8d35f63c78ecc202753efc
Mat2302 <mvfshoto@gmail.com>
2023-06-06 13:57:09 -0300

    Exercicio Calc

--- exercicio_textform/exercicio_textform/lib/calculadora.dart
+++ exercicio_textform/exercicio_textform/lib/calculadora.dart
@@ -10,6 +10,13 @@ class MyCalc extends StatefulWidget {
 }
 
 class _MyCalcState extends State<MyCalc> {
+  String campoUm = '';
+  String campoDois = '';
+  int resultado = 0;
+  double _resultado = 0;
+  TextEditingController campoTexto = TextEditingController();
+  TextEditingController campoTextoD = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +27,91 @@ class _MyCalcState extends State<MyCalc> {
       body: Center(
         child: Column(
           children: [
-            Padding(padding: EdgeInsets.only(top: 250, right: 30, left: 30)),
+            Padding(padding: EdgeInsets.all(15)),
             Text(
               "Operações para aprendizado do uso do Widget TextField",
               style: TextStyle(
                 fontSize: 18,
-
               ),
-            )
+            ),
+            TextField(
+              controller: campoTexto,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.blue[200],
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                labelText: 'Digite o valor 1',
+              ),
+              style: TextStyle(fontSize: 12, color: Colors.black),
+            ),
+            TextField(
+              controller: campoTextoD,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blue[200],
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  labelText: 'Digite o valor 2'),
+              style: TextStyle(fontSize: 12, color: Colors.black),
+            ),
+            Row(
+              children: [
+                SizedBox(height: 75, width: 25),
+                ElevatedButton(
+                    onPressed: () {
+                      double num1 = double.tryParse(campoTexto.text) ?? 0;
+                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                      double resultado = num1 + num2;
+                      setState(() {
+                        _resultado = resultado;
+                      });
+                    },
+                    child: Text("+")),
+                Padding(padding: EdgeInsets.only(right: 15)),
+                ElevatedButton(
+                    onPressed: () {
+                      double num1 = double.tryParse(campoTexto.text) ?? 0;
+                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                      double resultado = num1 - num2;
+                      setState(() {
+                        _resultado = resultado;
+                      });
+                    },
+                    child: Text("-")),
+                Padding(padding: EdgeInsets.only(right: 15)),
+                ElevatedButton(
+                    onPressed: () {
+                      double num1 = double.tryParse(campoTexto.text) ?? 0;
+                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                      double resultado = num1 * num2;
+                      setState(() {
+                        _resultado = resultado;
+                      });
+                    },
+                    child: Text("*")),
+                Padding(padding: EdgeInsets.only(right: 15)),
+                ElevatedButton(
+                    onPressed: () {
+                      double num1 = double.tryParse(campoTexto.text) ?? 0;
+                      double num2 = double.tryParse(campoTextoD.text) ?? 0;
+                      double resultado = num1 / num2;
+                      setState(() {
+                        _resultado = resultado;
+                      });
+                    },
+                    child: Text("/")),
+                Padding(padding: EdgeInsets.only(right: 15)),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _resultado = 0;
+                      });
+                    },
+                    child: Text("CE"))
+              ],
+            ),
+            Text('Resultado: $_resultado')
           ],
         ),
       ),
